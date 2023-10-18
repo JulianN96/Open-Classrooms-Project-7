9511c892594881433cd4bdca4dc6ae2bd5632278
Julian Norris <julian.norris96@gmail.com>
2023-10-18 16:55:31 +0200

    Fixed a typo in the auth section

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try{
     const token = req.headers.authorization.split(' ')[1];
-    const decodeToken = jwt.verify(token, 'random_token_secret');
+    const decodedToken = jwt.verify(token, 'random_token_secret');
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId){
       throw 'Invalid user ID';
